db2bb3a35fcc7b9245326c319afbb0722a25bba6
ue-shuston <samuelhuston@outlook.com>
2025-03-18 00:35:20 -0400

    Created general site layout and about and contact pages
    #
    - Closes #16
    - Issue #23

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,5 @@
 <template>
-  About Me
-
+  <h1>About Me</h1>
   My name is Samuel Huston. I am a Chattanooga, TN based graphic designer, web application
   developer, and programmer. I am a 2017 Chattanooga State graduate. I have an Associate’s degree in
   Digital Media Design and Production, with a concentration in Graphic Arts Technology.
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,8 +1,6 @@
 <template>
-  Contact Me
-
+  <h1>Contact Me</h1>
   Please feel free to access and contact me through any of the provided social media links below. Or
   email me at:
-
-  design@samuel-huston.com
+  <a href="mailto:design@samuel-huston.com">design@samuel-huston.com</a>
 </template>
